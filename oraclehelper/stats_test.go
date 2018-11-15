e7ffd52c4b5896100aae91dcbf6850599f9d7029
Björn Ahl <bjorn.ahl@gmail.com>
2018-11-15 09:34:55 +0100

    build v0.2.7 (#16)
    * rm ReadSchemaPref and fix setTablePref and adding test
    
    * Adding Changelog for 0.2.7

--- oraclehelper/stats_test.go
+++ oraclehelper/stats_test.go
@@ -1,6 +1,7 @@
 package oraclehelper
 
 import (
+	"fmt"
 	"log"
 	"testing"
 
@@ -11,7 +12,46 @@ var (
 	granularity = []string{"ALL", "AUTO", "DEFAULT", "PARTITION", "GLOBAL", "GLOBAL AND PARTITION", "SUBPARTITION"}
 )
 
-func TestStatsService(t *testing.T) {
+func TestStatsServiceTable(t *testing.T) {
+	tableName := acctest.RandStringFromCharSet(10, "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	c.DBClient.Exec(fmt.Sprintf("drop table system.%s", tableName))
+	c.DBClient.Exec(fmt.Sprintf("create table system.%s(col number)", tableName))
+
+	v := granularity[acctest.RandIntRange(0, 6)]
+	globalGranularity := granularity[acctest.RandIntRange(0, 6)]
+	for globalGranularity == v {
+		globalGranularity = granularity[acctest.RandIntRange(0, 6)]
+	}
+	t.Logf("got granularity: %s for table_name: %s and global: %s\n", v, tableName, globalGranularity)
+
+	c.StatsService.SetGlobalPre(ResourceStats{
+		Pname: "GRANULARITY",
+		Pvalu: globalGranularity,
+	})
+	resourceStats := ResourceStats{
+		Pname:   "GRANULARITY",
+		OwnName: "SYSTEM",
+		TaBName: tableName,
+		Pvalu:   v,
+	}
+	err := c.StatsService.SetTabPre(resourceStats)
+
+	if err != nil {
+		log.Fatalf("failed to stats, errormsg: %v\n", err)
+	}
+
+	tableGranularity, err := c.StatsService.ReadTabPref(resourceStats)
+	if err != nil {
+		log.Fatalf("failed to stats, errormsg: %v\n", err)
+	}
+
+	if resourceStats.Pvalu != tableGranularity.Pvalu {
+		t.Errorf("got %s; want %s\n", tableGranularity.Pvalu, resourceStats.Pvalu)
+	}
+	c.DBClient.Exec(fmt.Sprintf("drop table system.%s", tableName))
+}
+
+func TestStatsServiceGlobal(t *testing.T) {
 
 	v := granularity[acctest.RandIntRange(0, 6)]
 
