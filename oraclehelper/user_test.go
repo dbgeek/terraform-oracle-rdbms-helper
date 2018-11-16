46bf509ee4b1f036d5e985ab6a25cb5bbbf27ede
Björn Ahl <bjorn.ahl@gmail.com>
2018-11-16 22:52:41 +0100

    Expand user support (#17)
    * Adding vendoring hashicorp/terraform
    * Adding vendoring mattrobenolt/size
    * Adding vendoring golang.org/x/crypto
    * Adding quota support for user API
    * Adding account status support for user API
    * Adding profile support for user API

--- oraclehelper/user_test.go
+++ oraclehelper/user_test.go
@@ -5,17 +5,35 @@ import (
 )
 
 func TestUserService(t *testing.T) {
-
+	quota := make(map[string]string)
+	quota["USERS"] = "unlimited"
+	quota["SYSTEM"] = "10m"
+	quota["SYSAUX"] = "10G"
 	c.UserService.CreateUser(ResourceUser{Username: "TEST01"})
-
-	c.UserService.ModifyUser(ResourceUser{Username: "TEST01", DefaultTablespace: "SYSTEM"})
-
+	c.ProfileService.CreateProfile(ResourceProfile{Profile: "PP01"})
 	user, _ := c.UserService.ReadUser(ResourceUser{Username: "TEST01"})
+	if user.Profile != "DEFAULT" {
+		t.Errorf("want: %s, gott: %s", "DEFAULT", user.Profile)
+	}
+	c.UserService.ModifyUser(ResourceUser{Username: "TEST01", DefaultTablespace: "SYSTEM", Quota: quota, Profile: "PP01"})
 
+	user, _ = c.UserService.ReadUser(ResourceUser{Username: "TEST01"})
 	if "SYSTEM" != user.DefaultTablespace {
 		t.Errorf("%v; want %v\n", user.DefaultTablespace, "SYSTEM")
 	}
+	if user.Quota["SYSTEM"] != "10M" {
+		t.Errorf("gott: %s; want:%s\n", user.Quota["SYSTEM"], "10M")
+	}
+	if user.Quota["USERS"] != "unlimited" {
+		t.Errorf("%s; want %s\n", user.Quota["USERS"], "unlimited")
+	}
+	if user.Quota["SYSAUX"] != "10G" {
+		t.Errorf("%s; want %s\n", user.Quota["SYSAUX"], "10G")
+	}
+	if user.Profile != "PP01" {
+		t.Errorf("want: %s, gott: %s", "PP01", user.Profile)
+	}
 
 	c.UserService.DropUser(ResourceUser{Username: "TEST01"})
-
+	c.ProfileService.DeleteProfile(ResourceProfile{Profile: "PP01"})
 }
