827e1c245b877ea2ec0325b12b8917d953689fdd
Björn Ahl <bjorn.ahl@gmail.com>
2018-12-02 12:35:55 +0100

    Auto task (#22)
    * Add API to disable, enable, read, Managing Automated Database Maintenance Tasks
    
    * Tests for reading, disable, enable Managing Automated Database Maintenance Tasks

--- oraclehelper/oracle_helper.go
+++ oraclehelper/oracle_helper.go
@@ -30,6 +30,7 @@ type (
 		GrantService           *grantService
 		StatsService           *statsService
 		SchedulerWindowService *schedulerWindowService
+		AutoTaskService        *autoTaskService
 	}
 )
 
@@ -84,6 +85,7 @@ func NewClient(cfg Cfg) *Client {
 	c.GrantService = &grantService{client: c}
 	c.StatsService = &statsService{client: c}
 	c.SchedulerWindowService = &schedulerWindowService{client: c}
+	c.AutoTaskService = &autoTaskService{client: c}
 	c.DBVersion, _ = version.NewVersion(dBVersion)
 	log.Printf("[DEBUG] dbversion: %v", c.DBVersion)
 
